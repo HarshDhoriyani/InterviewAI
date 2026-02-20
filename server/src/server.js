bfd2dd00e193d80f0dcf6b49831dc7c3a4650c6f
Harsh Dhoriyani <harshdhoriyani2005@gmail.com>
2026-02-20 22:55:32 +0530

    Changes in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
+
 const http = require("http");
 const app = require("./app");
 const connectDB = require("./config/db");
 const { initSocket } = require("./core/websocket");
 
+connectDB();
+
 const server = http.createServer(app);
+
 initSocket(server);
 
+const PORT = process.env.PORT || 5000;
+
 server.listen(5000, () => {
-  console.log("Server + WebSocket running on port 5000");
-})
+  console.log(`Server running on port ${PORT}`);
+});
